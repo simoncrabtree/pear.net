3ac164d4b1a3b74f17534c11f685de6540977977
simoncrabtree <simon@crabsticks.net>
2014-01-30 17:25:38 +0000

    Added Home Controller and Hello World view

---
+++ src/pear.web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace pear.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
